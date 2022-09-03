5bb33c2b3198eaff20ee74b2efbd0e878fd7a655
slxfld <31144712+slxfld@users.noreply.github.com>
2022-09-03 16:35:13 +0200

    refresh every 3 hours

--- src/config/Config.java
+++ src/config/Config.java
@@ -1,13 +1,51 @@
 package config;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class Config {
 	
 	public static Properties properties = new Properties();
+	
+	public static Timer timer;
+
+	public static void init( ) {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				final String default_jre = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+				File jar;
+				try {
+					jar = new File(Config.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+
+					final ArrayList<String> command = new ArrayList<String>();
+					command.add(default_jre);
+					command.add("-jar");
+					command.add(jar.getPath());
+
+					final ProcessBuilder builder = new ProcessBuilder(command);
+					builder.start();
+					System.exit(0);
+				 } catch (URISyntaxException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}, (60000*60)*3, (60*60000)*3);
+	}
 	
 	public static void save() {
 		try {
