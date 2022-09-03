305eecda38f5950b241107edd4a23af490717d92
slxfld <31144712+slxfld@users.noreply.github.com>
2022-09-03 14:54:49 +0200

    add field labels for new task

--- src/window/panel/NewTaskPanel.java
+++ src/window/panel/NewTaskPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
@@ -35,17 +36,27 @@ public class NewTaskPanel extends JPanel {
 	}
 	
 	private void setup_ui_components() {
+		JLabel name_label = new JLabel("todo name");
+		name_label.setSize(150,25);
+		name_label.setLocation(25,0);
+		this.add(name_label);
+		
 		task_name_field = new JTextField();
-		task_name_field.setSize(200, 25);
+		task_name_field.setSize(300, 25);
 		task_name_field.setLocation(25,25);
 		if (!preset_name.isEmpty()) {
 			task_name_field.setText(preset_name);
 		}
 		this.add(task_name_field);
 		
+		JLabel days_label = new JLabel("days");
+		days_label.setSize(50,25);
+		days_label.setLocation(25,50);
+		this.add(days_label);
+		
 		task_repeat_days = new JSpinner();
 		task_repeat_days.setSize(125,25);
-		task_repeat_days.setLocation(25,70);
+		task_repeat_days.setLocation(25,75);
 		if (!preset_days.isEmpty()) {
 			task_repeat_days.setValue(Integer.parseInt(preset_days));
 		}
@@ -53,7 +64,7 @@ public class NewTaskPanel extends JPanel {
 		
 		save_button = new JButton("Save");
 		save_button.setSize(100,25);
-		save_button.setLocation(25,120);
+		save_button.setLocation(225,75);
 		save_button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
