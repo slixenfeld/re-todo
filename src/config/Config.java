13c96c6bfd09138f5ed6d7cbe465619ce629cf26
slxfld <31144712+slxfld@users.noreply.github.com>
2022-09-04 09:42:33 +0200

    crossplatform config file support

--- src/config/Config.java
+++ src/config/Config.java
@@ -48,7 +48,7 @@ public class Config {
 	
 	public static void save() {
 		try {
-			FileOutputStream stream = new FileOutputStream(System.getenv("APPDATA") + "/retodo.conf");
+			FileOutputStream stream = new FileOutputStream(getConfigPath() + "/retodo.conf");
 			properties.store(stream, null);
 			stream.flush();
 			stream.close();
@@ -59,11 +59,22 @@ public class Config {
 	
 	public static void load() {
 		try {
-			FileInputStream stream = new FileInputStream(System.getenv("APPDATA") + "/retodo.conf");
+			FileInputStream stream = new FileInputStream(getConfigPath() + "/retodo.conf");
 			properties.load(stream);
 			stream.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private static String getConfigPath() {
+		String os = (System.getProperty("os.name").toLowerCase());
+		if (os.contains("win")) {
+			return System.getenv("APPDATA");
+		} else if (os.contains("nux") || os.contains("nix") || os.contains("aix") || os.contains("mac") ) {
+			return "/home/" + System.getProperty("user.name") + "/.local/share/applications";
+		} else {
+			return "";
 		}
 	}
 }
