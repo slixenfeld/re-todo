afee0d23bc5f1cfa60122fcf8489b65488a44fbb
slxfld <31144712+slxfld@users.noreply.github.com>
2022-09-05 21:07:56 +0200

    Themes

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Desktop Application for managing reaccuring TODO tasks.
 
 ![UI](ui-screenshot.png "ui")
 
+# Themes
+The theme can be set as a JVM argument: lite,ocean,mars. <br/>
+java -jar re-todo.jar `theme`
+
 # Libraries Used
 * [Lombok](https://github.com/projectlombok/lombok)
 * [material-ui-swing](https://github.com/vincenzopalazzo/material-ui-swing)
--- src/launcher/Launch.java
+++ src/launcher/Launch.java
@@ -4,6 +4,7 @@ import javax.swing.UIManager;
 
 import config.Config;
 import mdlaf.MaterialLookAndFeel;
+import mdlaf.themes.JMarsDarkTheme;
 import mdlaf.themes.MaterialLiteTheme;
 import mdlaf.themes.MaterialOceanicTheme;
 import window.frame.MainFrame;
@@ -11,7 +12,18 @@ import window.frame.MainFrame;
 public class Launch {
 	public static void main(String args[]) {
         try {
-            UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme()));
+        	if (args.length > 0) {
+        		if (args[0].equalsIgnoreCase("lite")) {
+        			UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme()));
+        		} else if (args[0].equalsIgnoreCase("ocean")) {
+        			UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialOceanicTheme()));
+        		} else if (args[0].equalsIgnoreCase("mars")) {
+        			UIManager.setLookAndFeel(new MaterialLookAndFeel(new JMarsDarkTheme()));
+        		}
+        	}
+        	else {
+        		UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme()));
+        	}
         } catch (Exception e) {
         	System.out.println("setting look and feel failed: " + e.getMessage());
         }
