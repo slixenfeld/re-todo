a8f13133cee9ab834f2359bfcd2013d11c44b616
slxfld <31144712+slxfld@users.noreply.github.com>
2022-09-05 20:52:10 +0200

    themes & update Readme

--- src/launcher/Launch.java
+++ src/launcher/Launch.java
@@ -4,13 +4,14 @@ import javax.swing.UIManager;
 
 import config.Config;
 import mdlaf.MaterialLookAndFeel;
+import mdlaf.themes.MaterialLiteTheme;
 import mdlaf.themes.MaterialOceanicTheme;
 import window.frame.MainFrame;
 
 public class Launch {
 	public static void main(String args[]) {
         try {
-            UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialOceanicTheme()));
+            UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme()));
         } catch (Exception e) {
         	System.out.println("setting look and feel failed: " + e.getMessage());
         }
