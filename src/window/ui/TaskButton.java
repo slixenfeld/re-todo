982654bd01fc910752bace174d020c7f176a14d3
slxfld <31144712+slxfld@users.noreply.github.com>
2022-11-20 00:37:14 +0100

    repeating / non-repeating todos

--- src/window/ui/TaskButton.java
+++ src/window/ui/TaskButton.java
@@ -1,11 +1,13 @@
 package window.ui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import config.Config;
@@ -18,7 +20,9 @@ public class TaskButton extends JButton {
 	private static final long serialVersionUID = 1L;
 	String key;
 	int days;
+	boolean repeats;
 
+	private final JLabel repeat_label = new JLabel();
 	private final JPanel dueColorPanel = new JPanel();
 	
 	private void setDueColor(int days) {
@@ -38,6 +42,13 @@ public class TaskButton extends JButton {
 	}
 	
 	public void setDefaults() {
+		
+		repeat_label.setText((repeats) ? "R" : "1");
+		repeat_label.setLocation(5, 0);
+		repeat_label.setSize(30,25);
+		repeat_label.setFont(new Font("Verdana", Font.BOLD, 12));
+		add(repeat_label);
+		
 		this.setLayout(null);
 		this.setSize(300,25);
 		this.setEnabled(false);
@@ -48,7 +59,23 @@ public class TaskButton extends JButton {
 			this.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					Config.properties.put(key+"_last_date", LocalDate.now().toString());
+					
+						boolean repeats;
+						if (Config.properties.getProperty(key + "_repeating") == null)
+							repeats = true;
+						else
+							repeats = Config.properties.getProperty(key + "_repeating").equals("true");
+						
+						if (repeats) {
+							Config.properties.put(key+"_last_date", LocalDate.now().toString());
+						} else {
+							Config.properties.remove(key+"_days");
+							Config.properties.remove(key+"_last_date");
+							try {
+								Config.properties.remove(key+"_repeating");
+							} catch (Exception e2) {}	
+						}
+
 					Config.save();
 					MainFrameSingleton.getInstance().loadPanel(new MainPanel());
 				}
