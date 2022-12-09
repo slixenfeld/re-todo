d557a6066cf99c1ba6107210c0d40a32cfae1454
slxfld <31144712+slxfld@users.noreply.github.com>
2022-12-09 21:26:05 +0100

    show selected category

--- src/window/ui/TabButton.java
+++ src/window/ui/TabButton.java
@@ -1,9 +1,13 @@
 package window.ui;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import lombok.AllArgsConstructor;
 import window.frame.MainFrame;
@@ -16,14 +20,13 @@ public class TabButton extends JButton {
 	private static final long serialVersionUID = 1L;
 	
 	String name;
-
+	
 	public void setDefaults() {
 
 		this.setLayout(null);
 		this.setSize(100,25);
 		this.setEnabled(true);
-		this.setText(name);
-		
+
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -31,5 +34,23 @@ public class TabButton extends JButton {
 				MainFrameSingleton.getInstance().loadPanel(new MainPanel());
 			}
 		});
+	}
+	
+	public void addColorPanel() {
+		JPanel colorPanel = new JPanel();
+		System.out.println("name:" + name + " / " + MainFrame.currentTab);
+		if (name.equals(MainFrame.currentTab)) {
+			colorPanel.setSize(this.getWidth(),this.getHeight());
+			colorPanel.setBackground(new Color(50,50,50,40));
+			this.add(colorPanel);
+		}
+		
+		JLabel repeat_label = new JLabel();
+		repeat_label.setText(name);
+        repeat_label.setLocation(10, 0);
+        repeat_label.setSize(this.getWidth(),this.getHeight());
+        repeat_label.setFont(new Font("", Font.BOLD, 16));
+        add(repeat_label);
+		
 	}
 }
