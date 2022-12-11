aae2c88c5be9bb00c5158d75af8661816adb1c54
slxfld <31144712+slxfld@users.noreply.github.com>
2022-12-11 16:21:32 +0100

    MainFrame resizing

--- src/window/frame/MainFrame.java
+++ src/window/frame/MainFrame.java
@@ -2,27 +2,29 @@ package window.frame;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import config.Config;
+import window.panel.AbstractPanel;
 import window.panel.NewTaskPanel;
 import window.ui.WindowType;
 
 public class MainFrame extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
 	private static Point point = new Point();
 	public static MainFrame window;
-	private JPanel panel = new JPanel();
-	
-	public static String currentTab = "";	
+	private AbstractPanel panel = new AbstractPanel();
+
+	public static String currentTab = "";
 	public static WindowType currentWindowType = WindowType.MAIN;
-	
+
 	public MainFrame() {
 		setup_frame();
 	}
@@ -31,58 +33,87 @@ public class MainFrame extends JFrame {
 		setup_frame();
 		this.setLocation(location);
 	}
-	
-	public void loadPanel(JPanel panel) {
+
+	public void loadPanel(AbstractPanel panel) {
 		remove(this.panel);
 		this.panel = panel;
 		add(this.panel);
 	}
-	
+
 	private void setup_frame() {
 		setup_frame_settings();
 		add_listeners();
 		this.setVisible(true);
 	}
-		
+
 	private void setup_frame_settings() {
 		this.setTitle("re-todo");
 		this.setBackground(Color.black);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setType(Type.UTILITY);
 		this.setUndecorated(true);
-		this.setSize(0x171,0x200);
-		this.setResizable(false);
+		this.setSize(0x171, 0x200);
+		this.setResizable(true);
 	}
-	
-	private void add_listeners() {
-        this.addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                point.x = e.getX();
-                point.y = e.getY();
-            }
-        });
 
-        this.addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                Point p = MainFrameSingleton.getInstance().getLocation();
-                MainFrameSingleton.getInstance().setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
-            }
-        });
+	private void add_listeners() {
+		this.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				point.x = e.getX();
+				point.y = e.getY();
+			}
+		});
+
+		this.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				Point p = MainFrameSingleton.getInstance().getLocation();
+				MainFrameSingleton.getInstance().setLocation(p.x + e.getX() - point.x,
+						p.y + e.getY() - point.y);
+			}
+		});
+
+		this.addComponentListener(new ComponentListener() {
+
+			@Override
+			public void componentResized(ComponentEvent e) {
+				panel.resize();
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void componentShown(ComponentEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
 	}
-	
+
 	public static void editTask(String name) {
 
 		boolean repeats;
 		String category = "";
-		
+
 		if (Config.properties.getProperty(name + "_category") != null)
 			category = Config.properties.getProperty(name + "_category");
-		
+
 		if (Config.properties.getProperty(name + "_repeating") == null)
 			repeats = true;
 		else
 			repeats = Config.properties.getProperty(name + "_repeating").equals("true");
-			
-	MainFrameSingleton.getInstance().loadPanel(new NewTaskPanel(name, category, Config.properties.getProperty(name + "_days"), repeats));
+
+		MainFrameSingleton.getInstance().loadPanel(new NewTaskPanel(name, category,
+				Config.properties.getProperty(name + "_days"), repeats));
 	}
 }
--- src/window/panel/AbstractPanel.java
+++ src/window/panel/AbstractPanel.java
@@ -6,22 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import window.frame.MainFrameSingleton;
 
 public class AbstractPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
+
 	JButton new_button;
-	
+
 	JPanel thisPanel = this;
-	
+
+	JButton closeButton = new JButton("");
+	JLabel closeLabel = new JLabel("x");
+
+	public void resize() {
+		closeButton.setLocation(getWidth() - 41, 1);
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(new Color(21, 81, 89));
-		g.drawRect(0, 0, getWidth()-1, getHeight()-1);
+		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 		g.setColor(new Color(41, 164, 181));
 		g.drawRect(0, 0, getWidth(), getHeight());
 	}
@@ -29,12 +37,17 @@ public class AbstractPanel extends JPanel {
 	public void setup_ui_components() {
 		this.setLayout(null);
 		MainFrameSingleton.getInstance().setSize(this.getSize());
-	
-		JButton closeButton = new JButton("x");
+
 		closeButton.setFocusable(false);
-		closeButton.setSize(40,34);
-		closeButton.setLocation(getWidth()-41, 1);
+		closeButton.setSize(40, 34);
+		closeButton.setLocation(getWidth() - 41, 1);
+		closeButton.setLayout(null);
+
+		closeLabel.setBounds(16, 5, 20, 20);
+		closeButton.add(closeLabel);
+
 		closeButton.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (thisPanel.getClass() == MainPanel.class) {
